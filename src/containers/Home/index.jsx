185407f781602fe5b072fdb160a42fd8fd82a4ad
wps13 <willanepaiva@hotmail.com>
2019-04-12 21:14:38 -0300

    #10 - Modify home and comment style

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 
+import { Link } from "react-router-dom";
+
+import "./style.scss";
+
+const bookIMG = "../../public/images/black-bg.png";
+
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -8,7 +14,29 @@ class Home extends Component {
   }
 
   render() {
-    return <div>Hello!</div>;
+    return (
+      <div className="home">
+        <p id="title-home">Welcome to the Book Club!</p>
+        <div className="container-info-home">
+          <div className="block-info-home">
+            <p className="info-home">New Here?</p>
+            <Link to="/signup">
+              <p>Join Us</p>
+            </Link>
+          </div>
+          <div className="block-info-home">
+            <p className="info-home">Already here?</p>
+            <Link to="/login">Login</Link>
+          </div>
+        </div>
+        <div className="recent-book-home">
+          <p className="desc-recent-home">Recently registered books</p>
+          <Link to="/book">
+            <img src={bookIMG} alt="book img" className="book-img-home" />
+          </Link>
+        </div>
+      </div>
+    );
   }
 }
 
