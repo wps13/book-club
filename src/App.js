71a1c644748165b4b703cbef76549b617493cfef
wps13 <willanepaiva@hotmail.com>
2019-04-15 00:12:50 -0300

    #13 - create store with redux devTools

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+/* eslint-disable no-underscore-dangle */
 import React from "react";
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import "./App.css";
 import Routes from "./routes";
 
+import reducers from "./store/reducers";
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
 const App = () => (
   <div className="App">
-    <Routes />
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   </div>
 );
 
